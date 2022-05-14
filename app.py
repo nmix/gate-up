0d857b792a91334ab94274425a14f0d513b720d7
Nik Mikhaylichenko <nn.mikh@yandex.ru>
2022-05-14 20:09:42 +0300

    dev: get container list and request metrics

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ Scrape metrics from target services and push them to PushGateway
 """
 import docker
 import logging
+import os
 import requests
 
 logging.basicConfig(level="INFO")
@@ -12,25 +13,39 @@ def log(msg):
 
 docker_client = docker.DockerClient(base_url="unix://tmp/docker.sock")
 
-def current_container():
-    """
-    return Container instance for current app
-    """
-    hostname = open("/etc/hostname").readline()[:-1]
-    try:
-        container = docker_client.containers.get(hostname)
-    except docker.errors.NotFound:
-        RuntimeError("Not in docker?")
-    return container
+WORKING_DIR_LABEL = "com.docker.compose.project.working_dir"
+SCRAPE_LABEL = "com.github.nmix.gate-up.scrape"
 
 
 def project_containers():
     """
-    return filtered list of compose project containers
+    return list of compose project containers
     """
-    return []
+    # --- get current container
+    hostname = open("/etc/hostname").readline()[:-1]
+    try:
+        app_container = docker_client.containers.get(hostname)
+    except docker.errors.NotFound:
+        RuntimeError("Not in docker?")
+    # --- filter project containers by working project
+    working_dir = app_container.labels[WORKING_DIR_LABEL]
+    containers = docker_client.containers.list(
+            filters={"label": [f"{WORKING_DIR_LABEL}={working_dir}", SCRAPE_LABEL]}
+            )
+    containers = [ c for c in containers if c != app_container ]
+    return containers
 
+def get_container_scrape_params(container):
+    """
+    return service port and path for scrape
+    """
+    envd = dict([ e.split("=") for e in container.attrs["Config"]["Env"] ])
+    port = envd.get("SCRAPE_PORT", 80)
+    path = envd.get("SCRAPE_PATH", "/metrics")
+    return port, path
 
-c = current_container()
-log(c)
-log(c.labels)
+for container in project_containers():
+    log(container.name)
+    port, path = get_container_scrape_params(container)
+    response = requests.get(os.path.join(f"http://{container.name}:{port}", path))
+    log(response.text[:200])
