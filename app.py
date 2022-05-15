2c2c0561aeb0013c4a39fa5bcc4d9cb6fd36b0c7
Nik Mikhaylichenko <nn.mikh@yandex.ru>
2022-05-15 10:25:27 +0300

    dev: small style fixes

--- app.py
+++ app.py
@@ -3,18 +3,20 @@ Scrape metrics from target services and push them to PushGateway
 """
 import docker
 import logging
-import os
+# import os
 import requests
+from urllib.parse import urljoin
 
 logging.basicConfig(level="INFO")
-
-def log(msg):
-    logging.info(msg)
 
 docker_client = docker.DockerClient(base_url="unix://tmp/docker.sock")
 
 WORKING_DIR_LABEL = "com.docker.compose.project.working_dir"
 SCRAPE_LABEL = "com.github.nmix.gate-up.scrape"
+
+
+def log(msg):
+    logging.info(msg)
 
 
 def project_containers():
@@ -29,23 +31,26 @@ def project_containers():
         RuntimeError("Not in docker?")
     # --- filter project containers by working project
     working_dir = app_container.labels[WORKING_DIR_LABEL]
-    containers = docker_client.containers.list(
-            filters={"label": [f"{WORKING_DIR_LABEL}={working_dir}", SCRAPE_LABEL]}
-            )
-    containers = [ c for c in containers if c != app_container ]
+    labels = [f"{WORKING_DIR_LABEL}={working_dir}", SCRAPE_LABEL]
+    containers = docker_client.containers.list(filters={"label": labels})
+    containers = [c for c in containers if c != app_container]
     return containers
+
 
 def get_container_scrape_params(container):
     """
     return service port and path for scrape
     """
-    envd = dict([ e.split("=") for e in container.attrs["Config"]["Env"] ])
+    envd = dict([e.split("=") for e in container.attrs["Config"]["Env"]])
     port = envd.get("SCRAPE_PORT", 80)
     path = envd.get("SCRAPE_PATH", "/metrics")
     return port, path
 
+
 for container in project_containers():
-    log(container.name)
+    log("--------------------------")
     port, path = get_container_scrape_params(container)
-    response = requests.get(os.path.join(f"http://{container.name}:{port}", path))
+    url = urljoin(f"http://{container.name}:{port}", path)
+    log(url)
+    response = requests.get(url)
     log(response.text[:200])
