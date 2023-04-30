1648065ee474c22a3c8cbfb77b7eb39e9821ad39
Nik Mikhaylichenko <nn.mikh@yandex.ru>
2023-04-30 13:39:26 +0300

    dev: init swarm mode

--- app.py
+++ app.py
@@ -6,43 +6,78 @@ import logging
 import os
 import requests
 import time
+import dataclasses
+from urllib import parse
 
 from prometheus_client.parser import text_string_to_metric_families
 from prometheus_client.exposition import basic_auth_handler
 from prometheus_client import CollectorRegistry, push_to_gateway
-from urllib.parse import urljoin
 
+# --- default values
+DEFAULT_SCRAPE_PORT = 80
+DEFAULT_SCRAPE_PATH = '/metrics'
 
+# --- environment config
+DOCKER_BASE_URL = os.environ.get('DOCKER_BASE_URL', 'unix://tmp/docker.sock')
+JOB_PREFIX = os.environ.get('JOB_PREFIX', 'env')
+LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO')
 PUSHGATEWAY_URL = os.environ.get('PUSHGATEWAY_URL', 'http://pushgateway:9091')
 PUSHGATEWAY_BASIC_AUTH_USERNAME = os.environ.get(
         'PUSHGATEWAY_BASIC_AUTH_USERNAME', '')
 PUSHGATEWAY_BASIC_AUTH_PASSWORD = os.environ.get(
         'PUSHGATEWAY_BASIC_AUTH_PASSWORD', '')
-
 SCRAPE_LABEL = os.environ.get('SCRAPE_LABEL', 'com.github.nmix.gate-up.scrape')
+# --- @TODO: refactor required
 SCRAPE_INTERVAL = int(os.environ.get('SCRAPE_INTERVAL', 5))
 if SCRAPE_INTERVAL < 1:
     SCRAPE_INTERVAL = 1
 
-DOCKER_BASE_URL = os.environ.get('DOCKER_BASE_URL', 'unix://tmp/docker.sock')
-docker_client = docker.DockerClient(base_url=DOCKER_BASE_URL)
-
-JOB_PREFIX = os.environ.get('JOB_PREFIX', 'env')
-
-LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO')
 
 logging.basicConfig(level=LOG_LEVEL)
+logging.info('Gate UP!')
 
-logging.info(f'Gate UP!')
 
+@dataclasses.dataclass
+class Service():
+    '''Service with metrics, e.g. docker container or swarm task
 
-def project_containers():
+    Attributes:
+    name - dns name (container or task name)
+    labels - labels dict, e.g.
+        {
+            'com.docker.stack.namespace': 'gateup',
+            'com.github.nmix.gate-up.scrape': ''
+            }
+    env - environment variables list, e.g. ['SCRAPE_PORT=9100']
     '''
-    return list of compose project containers
-    '''
-    containers = docker_client.containers.list(
-            filters={'label': [SCRAPE_LABEL]})
-    return containers
+    name: str
+    labels: dict = dataclasses.field(default_factory=dict)
+    env: list[int] = dataclasses.field(default_factory=list)
+
+    @property
+    def url(self) -> str:
+        '''Service url for scrape metrics'''
+        envd = dict([e.split('=') for e in self.env])
+        port = envd.get('SCRAPE_PORT', DEFAULT_SCRAPE_PORT)
+        path = envd.get('SCRAPE_PATH', DEFAULT_SCRAPE_PATH)
+        return parse.urljoin(f'http://{self.name}:{port}', path)
+
+
+def host_services() -> list[Service]:
+    '''return containers services on host'''
+    docker_client = docker.DockerClient(base_url=DOCKER_BASE_URL)
+    services = []
+    for container in docker_client.containers.list():
+        service = Service(name=container.name,
+                          labels=container.attrs['Config']['Labels'],
+                          env=container.attrs['Config']['Env'])
+        services.append(service)
+    return services
+
+
+def swarm_task_services() -> list[Service]:
+    '''return task services'''
+
 
 
 def get_container_scrape_params(container):
@@ -91,24 +126,28 @@ def push_gateway_handler(url, method, timeout, headers, data):
             PUSHGATEWAY_BASIC_AUTH_PASSWORD)
 
 
-while True:
-    time.sleep(SCRAPE_INTERVAL)
-    containers = project_containers()
-    if len(containers) == 0:
-        logging.info('There are no project containers')
-        continue
-    for container in containers:
-        # --- specify the metrics url
-        port, path = get_container_scrape_params(container)
-        url = urljoin(f'http://{container.name}:{port}', path)
-        logging.info(f'scrape metrics from {url}')
-        # ---
-        registry = CollectorRegistry()
-        registry.register(collector(url))
-        # --- push metrics to pushgateway
-        push_to_gateway(
-                PUSHGATEWAY_URL,
-                job=f'{JOB_PREFIX}-{container.name}',
-                registry=registry,
-                handler=push_gateway_handler)
+def main():
+    while True:
+        time.sleep(SCRAPE_INTERVAL)
+        # containers = project_containers()
+        # if len(containers) == 0:
+        #     logging.info('There are no project containers')
+        #     continue
+        # for container in containers:
+        #     # --- specify the metrics url
+        #     port, path = get_container_scrape_params(container)
+        #     url = urljoin(f'http://{container.name}:{port}', path)
+        #     logging.info(f'scrape metrics from {url}')
+        #     # ---
+        #     registry = CollectorRegistry()
+        #     registry.register(collector(url))
+        #     # --- push metrics to pushgateway
+        #     push_to_gateway(
+        #             PUSHGATEWAY_URL,
+        #             job=f'{JOB_PREFIX}-{container.name}',
+        #             registry=registry,
+        #             handler=push_gateway_handler)
 
+
+if __name__ == '__main__':
+    main()
---
+++ test_app.py
@@ -0,0 +1,19 @@
+import app
+import pytest
+
+
+@pytest.mark.parametrize(
+        'name, env, url',
+        [
+            ('foo', [], 'http://foo:80/metrics'),
+            ('bar', ['SCRAPE_PORT=9999'], 'http://bar:9999/metrics'),
+            (
+                'foobar',
+                ['SCRAPE_PORT=8888', 'SCRAPE_PATH=/observability/metrics'],
+                'http://foobar:8888/observability/metrics'
+                ),
+            ]
+        )
+def test_service_url(name, env, url):
+    service = app.Service(name, env=env)
+    assert service.url == url
